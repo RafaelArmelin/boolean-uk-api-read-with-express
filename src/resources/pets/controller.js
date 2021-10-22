2df36f402fa08d4c2022d01ac4f2602afd665736
Rafael Armelin <raffael.armelin@gmail.com>
2021-10-22 15:21:03 +0100

    chenges

--- src/resources/pets/controller.js
+++ src/resources/pets/controller.js
@@ -2,6 +2,16 @@ const db = require("../../utils/database");
 
 const getOneById = async (req, res) => {
   console.log("hereeee");
+  const getOneByIdSQL = "SELECT * FROM pets WHERE id = $1";
+
+  try {
+    const result = await db.query(getOneByIdSQL, [req.params.id]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("[ERROR] getOneById: ", { error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const createOne = async (req, res) => {
@@ -32,7 +42,25 @@ const createOne = async (req, res) => {
   }
 };
 
+const getAll = async (req, res) => {
+  const { microship } = req.query;
+  let getAllSQL = "SELECT * FROM pets";
+
+  if (microship) {
+    getAllSQL = "${getAllSQL} WHERE microship = false";
+  }
+
+  try {
+    const result = await db.query(getAllSQL);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("[ERROR] getAll: ", { error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createOne,
   getOneById,
+  getAll,
 };
